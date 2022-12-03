d19b1937d1e1702cffce7186f804244ed1e2dd24
Modassir Afzal <modassir2024@gmail.com>
2022-12-03 19:26:00 +0530

    Version 1.1

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
-setuptools.setup(name='probability_distributions',
-      version='1.0',
+setuptools.setup(name='probability_distributions_functions',
+      version='1.1',
       description='Gaussian and Binomial Probability distributions',
-      packages=['probability_distributions'],
+      packages=['probability_distributions_functions'],
       author='Modassir Afzal',
       author_email='modassir2024@gmail.com',
       zip_safe=False,
-      url = 'https://github.com/Moddy2024/Probability-Distribution',
+      url = 'https://github.com/Moddy2024/Probability-Distribution-Functions',
     install_requires=[
         "matplotlib>=3.6.0"
     ]
